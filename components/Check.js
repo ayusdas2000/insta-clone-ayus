933b0d954066f52753265c81bc2aa3ccfb842918
Ayus Das <40837456+ayusdas2000@users.noreply.github.com>
2022-07-27 20:47:02 +0530

    some changes done

--- components/Check.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Check() {
-  return (
-    <div>check done with component</div>
-  )
-}
-
-export default Check
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,9 @@ const Home: NextPage = () => {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
-        <title>Instagram 2.0 Youtube</title>
+        <title>Instagram 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/*Header*/}
       <Modal/>
       <Header/>
       <Feed/>      
